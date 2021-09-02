a1b81ef4f6f0788f5155cafa7bde2c65086dcf46
shashi <shahsikrishna129@gmail.com>
2021-09-02 23:09:45 +0530

    added iam

---
+++ Code/iam/var.tf
@@ -0,0 +1,11 @@
+#variable for IAM user name
+variable "user-name" {}
+
+#variable for IAM group name
+variable "iam-group-name" {}
+
+#variable for policy name
+variable "policy-name" {}
+
+#variable date to be attached every time
+variable "date" {}
